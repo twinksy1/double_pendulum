74dfa80bc9848fc8bc82c2d2dd1502414f9b1431
twinksy1 <twinksy1@gmail.com>
2020-02-14 10:38:33 -0800

    Added physics & rendering of pendulums

--- main.c
+++ main.c
@@ -22,11 +22,19 @@ struct X11_wrapper {
 };
 
 struct pendulum {
+	float x;
+	float y;
+	float mass;
+	float angle;
+	float length;
+	float vel;
+	float acc;
 };
 
 //GLOBAL OBJECTS///////
 struct X11_wrapper x11;
 struct global g;
+struct pendulum p1, p2;
 ///////////////////////
 
 //X11 Function Definitions/////////////////////////////////////////////////////////
@@ -41,6 +49,15 @@ void setup()
 {
 	g.xres = 1280;
 	g.yres = 1080;
+
+	p1.length = 350.0;
+	p2.length = 220.0;
+	p1.angle = -90.0;
+	p2.angle = 90.0;
+	p1.mass = 40.0;
+	p2.mass = 20.0;
+	p1.vel = 0.005;
+	p2.vel = 0.005;
 
 	XSetWindowAttributes attributes;
 	XdbeBackBufferAttributes* back_attr;
@@ -158,6 +175,27 @@ void clear_screen()
 	XFillRectangle(x11.dpy, x11.back_buffer, x11.gc, 0, 0, g.xres, g.yres);
 }
 
+void set_color(int r, int g, int b)
+{
+	unsigned long cref = 0L;
+	cref += r;
+	cref <<= 8;
+	cref += g;
+	cref <<= 8;
+	cref += b;
+	XSetForeground(x11.dpy, x11.gc, cref);
+}
+
+void draw_point(int x, int y)
+{
+	XDrawPoint(x11.dpy, x11.back_buffer, x11.gc, x, y);
+}
+
+void draw_line(int x0, int y0, int x1, int y1)
+{
+	XDrawLine(x11.dpy, x11.back_buffer, x11.gc, x0, y0, x1, y1);
+}
+
 void destroy()
 {
 	if(!XdbeDeallocateBackBufferName(x11.dpy, x11.back_buffer)) {
@@ -170,17 +208,75 @@ void destroy()
 //END X11 Defintions////////////////////////////////////////////////////
 
 //Physics
-void physics() {}
+void physics()
+{
+	const float grav = 9.8;
+	const float OFFSETX = (float)g.xres / 2.0;
+	const float OFFSETY = 100.0;
+	
+	p1.x = sin(p1.angle) * p1.length + OFFSETX;
+	p1.y = cos(p1.angle) * p1.length + OFFSETY;
+	
+	p2.x = sin(p2.angle) * p2.length + p1.x;
+	p2.y = cos(p2.angle) * p2.length + p1.y;
+
+	float numerator = -grav*(2*p1.mass+p2.mass)*sin(p1.angle)-p2.mass*grav*sin(p1.angle-2*p2.angle)-
+		2*sin(p1.angle-p2.angle)*p2.mass*(p2.vel*p2.vel*p2.length+p1.vel*p1.vel*p1.length*
+				cos(p1.angle-p2.angle));
+	float denominator = p1.length*(2*p1.mass+p2.mass-p2.mass*cos(2*p1.angle-2*p2.angle));
+	p1.acc = numerator / denominator;
+	p1.vel += p1.acc;
+	if(p1.vel > 0.15) p1.vel = 0.15;
+	if(p1.vel < -0.15) p1.vel = -0.15;
+	p1.angle += p1.vel;
+
+	numerator = 2*sin(p1.angle-p2.angle)*(p1.vel*p1.vel*p1.length*(p1.mass+p2.mass)+grav*(p1.mass+p2.mass)*
+			cos(p1.angle)+p2.vel*p2.vel*p2.length*p2.mass*cos(p1.angle-p2.angle));
+	denominator = p2.length*(2*p1.mass+p2.mass-p2.mass*cos(2*p1.angle-2*p2.angle));
+	p2.acc = numerator / denominator;
+	p2.vel += p2.acc;
+	if(p2.vel > 0.2) p2.vel = 0.2;
+	if(p2.vel < -0.2) p2.vel = -0.2;
+	p2.angle += p2.vel;
+
+	usleep(2000);
+}
 
 //Rendering
 void render()
 {
 	clear_screen();
+	const float OFFSETX = (float)g.xres / 2.0;
+	const float OFFSETY = 100.0;
+	set_color(255, 0, 0);
+	
+	draw_line(OFFSETX, OFFSETY, p1.x, p1.y);
+	draw_line(p1.x, p1.y, p2.x, p2.y);
+
+	for(int i=0; i<g.yres; i++) {
+		for(int j=0; j<g.xres; j++) {
+			float xdiff = j - p1.x;
+			float ydiff = i - p1.y;
+			float dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+			if(dist <= p1.mass) {
+				(dist == p1.mass) ? set_color(0, 0, 0) : set_color(255, 0, 0);
+				draw_point(j, i);
+			}
+			xdiff = j - p2.x;
+			ydiff = i - p2.y;
+			dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+			if(dist <= p2.mass) {
+				(dist == p2.mass) ? set_color(0, 0, 0) : set_color(0, 0, 255);
+				draw_point(j, i);
+			}
+		}
+	}
 }
 
 //Main Loop
 int main()
 {
+	srand(time(NULL));
 	setup();
 	int done = 0;
 	while(!done) {
